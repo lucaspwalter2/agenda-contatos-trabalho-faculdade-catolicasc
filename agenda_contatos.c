9737b17abe508bdb333e29aaf477d244b6f5359c
Lucas Pereira Walter <63117272+lucaspwalter@users.noreply.github.com>
2026-06-29 21:27:11 -0300

    Completa bonus da agenda de contatos

--- agenda_contatos.c
+++ agenda_contatos.c
@@ -1,6 +1,10 @@
 #include <stdio.h>
 #include <string.h>
 
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
 #define MAX 100
 #define TAM_NOME 80
 #define TAM_TELEFONE 30
@@ -16,6 +20,30 @@ typedef struct {
 
 Contato contatos[MAX];
 int quantidade = 0;
+
+void gotoxy(int x, int y) {
+#ifdef _WIN32
+    COORD posicao;
+
+    posicao.X = x - 1;
+    posicao.Y = y - 1;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), posicao);
+#else
+    printf("\033[%d;%dH", y, x);
+#endif
+}
+
+void limparTela() {
+    printf("\033[2J");
+    gotoxy(1, 1);
+}
+
+void cabecalho() {
+    limparTela();
+    printf("=====================================\n");
+    printf("        AGENDA DE CONTATOS\n");
+    printf("=====================================\n");
+}
 
 void salvar() {
     FILE *arquivo;
@@ -35,6 +63,21 @@ void salvar() {
     fclose(arquivo);
 }
 
+void ordenar() {
+    int i, j;
+    Contato auxiliar;
+
+    for (i = 0; i < quantidade - 1; i++) {
+        for (j = i + 1; j < quantidade; j++) {
+            if (strcmp(contatos[i].nome, contatos[j].nome) > 0) {
+                auxiliar = contatos[i];
+                contatos[i] = contatos[j];
+                contatos[j] = auxiliar;
+            }
+        }
+    }
+}
+
 void carregar() {
     FILE *arquivo;
 
@@ -51,6 +94,7 @@ void carregar() {
     }
 
     fclose(arquivo);
+    ordenar();
 }
 
 void incluir() {
@@ -72,6 +116,7 @@ void incluir() {
     scanf(" %59[^\n]", contatos[quantidade].cidade);
 
     quantidade++;
+    ordenar();
     salvar();
     printf("Contato cadastrado.\n");
 }
@@ -117,6 +162,43 @@ void consultar() {
     }
 }
 
+void alterar() {
+    char busca[TAM_NOME];
+    int i, achou;
+
+    achou = 0;
+    printf("\nDigite o nome exato do contato que deseja alterar: ");
+    scanf(" %79[^\n]", busca);
+
+    for (i = 0; i < quantidade; i++) {
+        if (strcmp(contatos[i].nome, busca) == 0) {
+            printf("\nContato encontrado. Digite os novos dados.\n");
+
+            printf("Nome: ");
+            scanf(" %79[^\n]", contatos[i].nome);
+
+            printf("Telefone: ");
+            scanf(" %29[^\n]", contatos[i].telefone);
+
+            printf("E-mail: ");
+            scanf(" %79[^\n]", contatos[i].email);
+
+            printf("Cidade: ");
+            scanf(" %59[^\n]", contatos[i].cidade);
+
+            ordenar();
+            salvar();
+            achou = 1;
+            printf("Contato alterado.\n");
+            break;
+        }
+    }
+
+    if (achou == 0) {
+        printf("Contato nao encontrado.\n");
+    }
+}
+
 void excluir() {
     char busca[TAM_NOME];
     int i, j, achou;
@@ -145,13 +227,43 @@ void excluir() {
 }
 
 void menu() {
-    printf("\n===== AGENDA DE CONTATOS =====\n");
+    cabecalho();
     printf("1 - Incluir contato\n");
     printf("2 - Listar contatos\n");
     printf("3 - Consultar contato pelo nome\n");
     printf("4 - Excluir contato\n");
     printf("5 - Sair\n");
+    printf("6 - Alterar contato\n");
+    printf("7 - Ordenar contatos por nome\n");
+    printf("\n");
+    printf("Total de contatos: %i\n", quantidade);
+    printf("-------------------------------------\n");
     printf("Opcao: ");
+}
+
+void pausar() {
+    char enter;
+
+    printf("\nPressione ENTER para voltar ao menu...");
+    scanf("%c", &enter);
+    scanf("%c", &enter);
+}
+
+void ordenarMenu() {
+    ordenar();
+    salvar();
+    printf("\nContatos ordenados em ordem alfabetica.\n");
+}
+
+int lerOpcao() {
+    int opcao;
+
+    if (scanf("%i", &opcao) != 1) {
+        scanf("%*[^\n]");
+        return 0;
+    }
+
+    return opcao;
 }
 
 int main() {
@@ -162,26 +274,39 @@ int main() {
 
     while (opcao != 5) {
         menu();
-        scanf("%i", &opcao);
+        opcao = lerOpcao();
 
         switch (opcao) {
             case 1:
                 incluir();
+                pausar();
                 break;
             case 2:
                 listar();
+                pausar();
                 break;
             case 3:
                 consultar();
+                pausar();
                 break;
             case 4:
                 excluir();
+                pausar();
                 break;
             case 5:
                 printf("Programa encerrado.\n");
                 break;
+            case 6:
+                alterar();
+                pausar();
+                break;
+            case 7:
+                ordenarMenu();
+                pausar();
+                break;
             default:
                 printf("Opcao invalida.\n");
+                pausar();
         }
     }
 
